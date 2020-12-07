9f6c6ba800b654acfa827cc1f1c84e0846183c36
Nayla <nayla.mar2@gmail.com>
2020-12-06 23:56:58 -0300

    confetti!

--- containers/RandomBar/index.js
+++ containers/RandomBar/index.js
@@ -1,6 +1,9 @@
 import Container from './styled';
+//import React from 'react'
 import {Grid, Col, Inner, CardBar, InfoProfile} from 'components'
 import { useProtected } from 'lib/useProtected';
+//import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 
 const RandomBar = () => {
 
@@ -8,12 +11,19 @@ const RandomBar = () => {
     const auth = useProtected();
     if (!auth.user) return null;
     
+    //const { width, height } = useWindowSize();
+    const width = 1400;
+    const height = 720;
 
     return (
         <Inner>
             <Grid>
                 <Col desktop={12} tablet={12} mobile={12}>
                     <h1>RandomBar</h1>
+                    <Confetti
+                    width={width}
+                    height={height}
+                    />
                 </Col>
                 
             </Grid>
